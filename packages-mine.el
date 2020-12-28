bfaaaccf2c81791dd51734d5a8543582d4ad4208
Ryan Wilcox <rwilcox@wilcoxd.com>
2020-12-28 10:57:30 -0500

    let's try to run the cloud theme

--- packages-mine.el
+++ packages-mine.el
@@ -4,8 +4,14 @@
 (use-package rainbow-blocks)
 (use-package imake)
 (use-package devdocs)
-(use-package color-theme-sanityinc-solarized)
-(use-package base16-theme)
+                                        ; (use-package color-theme-sanityinc-solarized)
+
+    (use-package cloud-theme
+        :ensure t
+        :config
+        (load-theme 'cloud t))
+
+; (use-package base16-theme)
 (use-package persistent-scratch)
 (use-package literate-calc-mode)
 (use-package swiper)
