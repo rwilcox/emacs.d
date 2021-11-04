45da99de5b3c75704852ec290bf8873b1a7a12a3
Ryan Wilcox <rwilcox@wilcoxd.com>
2021-11-04 14:18:31 -0400

    allow machine specific configs to be loaded

--- boot-after-packages.el
+++ boot-after-packages.el
@@ -4,3 +4,7 @@
 (require 'graphviz-mine)
 (require 'python-mine)
 (require 'wwtime-mine)
+
+; sometimes machines have specific configs, meaning locations of supporting binaries etc etc
+; in my setup these are named machine-configs/machine-name-any-dot-here-are-replaced-with-dashes-config.el
+(require (intern (replace-regexp-in-string (regexp-quote "\.") "-" (format "%s-config" system-name))))
---
+++ machine-configs/home-lan-config.el
@@ -0,0 +1,1 @@
+(provide 'home-lan-config)
