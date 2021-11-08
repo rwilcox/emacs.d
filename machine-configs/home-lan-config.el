6552559addb57262c60e766030244c7dad6a9cb5
Ryan Wilcox <rwilcox@wilcoxd.com>
2021-11-07 20:32:01 -0500

    fix home machine config; add new machine java config

--- machine-configs/home-lan-config.el
+++ machine-configs/home-lan-config.el
@@ -5,7 +5,4 @@
 (setq java-11-binpath (concat "/Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home/bin/:" eshell-path-env) )
 
 (setq java-8-home     "/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home")
-(setq java-8-binpath  (concat "/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home/bin: eshell-path-env))
-
-
-)
+(setq java-8-binpath  (concat "/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home/bin:" eshell-path-env))
--- machine-configs/virotutis-lan-config.el
+++ machine-configs/virotutis-lan-config.el
@@ -1,1 +1,10 @@
 (provide 'virotutis-lan-config)
+
+(provide 'home-lan-config)
+
+
+(setq java-11-home    "/Library/Java/JavaVirtualMachines/openjdk11-zulu/Contents/Home")
+(setq java-11-binpath (concat "/Library/Java/JavaVirtualMachines/openjdk11-zulu/Contents/Home/bin/:" eshell-path-env) )
+
+(setq java-8-home     "/Library/Java/JavaVirtualMachines/openjdk8-zulu/Contents/Home")
+(setq java-8-binpath  (concat "/Library/Java/JavaVirtualMachines/openjdk8-zulu/Contents/Home/bin: eshell-path-env"))
