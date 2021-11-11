3dad42d4ff91b57603174e50848cc2dbd2bf2b88
Ryan Wilcox <rwilcox@wilcoxd.com>
2021-11-11 09:57:33 -0500

    specify path for kotlin LSP, because thanks Gradle

--- machine-configs/home-lan-config.el
+++ machine-configs/home-lan-config.el
@@ -6,3 +6,5 @@
 
 (setq java-8-home     "/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home")
 (setq java-8-binpath  (concat "/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home/bin:" eshell-path-env))
+
+(setq lsp-clients-kotlin-server-executable "/Users/aias/bin/install/server/bin/kotlin-language-server")
