4edf9f150e575910f83ed2f2ffe0f72ab1b58a22
Ryan Wilcox <rwilcox@wilcoxd.com>
2022-03-31 13:55:34 -0400

    make line mode silly short, not long

--- boot-after-packages.el
+++ boot-after-packages.el
@@ -8,3 +8,14 @@
 ; sometimes machines have specific configs, meaning locations of supporting binaries etc etc
 ; in my setup these are named machine-configs/machine-name-any-dot-here-are-replaced-with-dashes-config.el
 (require (intern (replace-regexp-in-string (regexp-quote "\.") "-" (format "%s-config" system-name))))
+
+  (setq-default mode-line-format
+          (list
+           ;; value of `mode-name'
+           " %m: "
+           ;; value of current buffer name
+           "buffer '%b' (%&) "
+           ;; value of current line number
+           "@ L: %l C: %C "
+           "(%Z)"
+          ))
