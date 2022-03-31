d55c58ff65d79f24cf4b164d8a05b8425ada4a0f
Ryan Wilcox <rwilcox@wilcoxd.com>
2022-03-31 13:54:21 -0400

    add indent/unindent functions

--- functional_areas/editing-extensions.el
+++ functional_areas/editing-extensions.el
@@ -6,5 +6,21 @@
 
   (setq indent-tabs-mode t)
                                         ; https://dougie.io/emacs/indentation/#functions-for-enablingdisabling-tabs
+  )
 
-)
+(defun shift-region (distance)
+  (let ((mark (mark)))
+    (save-excursion
+      (indent-rigidly (region-beginning) (region-end) distance)
+      (push-mark mark t t)
+      ;; Tell the command loop not to deactivate the mark
+      ;; for transient mark mode
+      (setq deactivate-mark nil))))
+
+(defun shift-right-region ()  ; seriously, don't run this on not-a-region
+  (interactive)
+  (shift-region 2))
+
+(defun shift-left-region ()
+  (interactive)
+  (shift-region -2))
