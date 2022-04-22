9811ddb4e9bd7fdfc8e7164a5185acc9ef0a9a84
Ryan Wilcox <rwilcox@wilcoxd.com>
2022-04-22 19:35:05 -0400

    pull plantuml mode into the unifying embrace of C-c r p structure

--- functional_areas/plantuml-mine.el
+++ functional_areas/plantuml-mine.el
@@ -2,3 +2,9 @@
 
  (setq plantuml-jar-path "~/Applications/plantuml.jar")
  (setq plantuml-default-exec-mode 'jar)
+
+(defun plantuml-setup-mine ()
+  (local-set-key (kbd "C-c r p") 'plantuml-preview)
+ )
+
+(add-hook 'plantuml-mode-hook 'plantuml-setup-mine)
