746757f087f510b41b25958e33871ec5ed2756af
Ryan Wilcox <rwilcox@wilcoxd.com>
2022-11-14 16:51:46 -0500

    utilities to make root level splits

--- functional_areas/window-extensions.el
+++ functional_areas/window-extensions.el
@@ -5,3 +5,18 @@
 ;; and C-x SHIFT o to go to the "previous" window
 
 (global-set-key (kbd "C-x O") (lambda () (interactive) (other-window -1)))
+
+;; from https://www.masteringemacs.org/article/demystifying-emacs-window-manager
+(defun mp-root-split-below (arg)
+  "Split window below from the parent or from root with ARG."
+  (interactive "P")
+  (split-window (if arg (frame-root-window)
+                  (window-parent (selected-window)))
+                nil 'below nil))
+
+(defun mp-root-split-right (arg)
+  "Split window below from the parent or from root with ARG."
+  (interactive "P")
+  (split-window (if arg (frame-root-window)
+                  (window-parent (selected-window)))
+                nil 'right nil))
