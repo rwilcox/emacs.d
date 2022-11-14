b151299c8d89de01c9c00d0b91d0e8c9cde62e07
Ryan Wilcox <rwilcox@wilcoxd.com>
2022-11-14 10:02:21 -0500

    mised file

---
+++ theme-mine.el
@@ -0,0 +1,14 @@
+(provide 'theme-mine)
+
+ ; current status of theme:
+; using mousemacs theme, so set in mousemacs/mousemacs-theme to load correct built in theme
+
+; but for when I'm not, set it here...
+                                        ; (use-package color-theme-sanityinc-solarized)
+
+;    (use-package cloud-theme
+;        :ensure t
+;        :config
+;        (load-theme 'cloud t))
+
+; (use-package base16-theme)
