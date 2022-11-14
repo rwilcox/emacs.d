d4a6f1d672358bccf9ed5162cf1b9a2ead1cc51a
Ryan Wilcox <rwilcox@wilcoxd.com>
2022-11-13 21:03:03 -0500

    Use the yellow mousemacs theme, it's grown on me a bit

--- init.el
+++ init.el
@@ -14,10 +14,11 @@
 
 ;; load mousemacs
 (load (concat mousemacs-path "mousemacs-core.el"))
-; (load (concat mousemacs-path "mousemacs-theme.el"))
+ (load (concat mousemacs-path "mousemacs-theme.el"))
 (load (concat mousemacs-path "mousemacs-keybinding.el"))
 (load (concat mousemacs-path "mousemacs-etc.el"))
 
 (load (concat user-emacs-directory "packages-mine.el"))
 (load (concat user-emacs-directory "custom.el"))
+(load (concat user-emacs-directory "theme-mine.el"))
 (load (concat user-emacs-directory "boot-after-packages.el"))
