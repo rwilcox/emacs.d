20596cc1254d7f4a0253d16b52854451a98b9fe1
Ryan Wilcox <rwilcox@wilcoxd.com>
2023-08-11 19:36:23 -0400

    configs for my new laptop

---
+++ machine-configs/airiel-local-config.el
@@ -0,0 +1,12 @@
+(provide 'Airiel-local-config)
+
+
+(setq java-11-home    "/Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home")
+(setq java-11-binpath (concat "/Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home/bin/:" eshell-path-env) )
+
+(setq java-8-home     "/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home")
+(setq java-8-binpath  (concat "/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home/bin:" eshell-path-env))
+
+(setq lsp-clients-kotlin-server-executable "/Users/aias/bin/install/server/bin/kotlin-language-server")
+
+(setq rg-executable "/opt/local/bin/rg")
