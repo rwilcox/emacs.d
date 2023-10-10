823cd81582f0331df30a06170eac9be8ef8e51b3
Ryan Wilcox <rwilcox@wilcoxd.com>
2023-10-10 17:14:00 -0400

    note how treesitter really works

--- functional_areas/treesitter-mine.el
+++ functional_areas/treesitter-mine.el
@@ -16,7 +16,9 @@
      (toml "https://github.com/tree-sitter/tree-sitter-toml")
      (tsx "https://github.com/tree-sitter/tree-sitter-typescript" "master" "tsx/src")
      (typescript "https://github.com/tree-sitter/tree-sitter-typescript" "master" "typescript/src")
-     (yaml "https://github.com/ikatyang/tree-sitter-yaml")))
+     (yaml "https://github.com/ikatyang/tree-sitter-yaml")
+     (hcl "https://github.com/MichaHoffmann/tree-sitter-hcl")
+     ))
 
 ; HOW TO ADD A NEW TREE-SITTER LANGUAGE
 ;   #1: find a repo and add it to this list
@@ -24,7 +26,9 @@
 ;   #3: It's under LANGUAGE-ts-mode (the TS is for TreeSitter, not Teh Suck)
 ;   #4: If you like it, probably want to remap it so things don't know the difference
 ; FROM: https://www.masteringemacs.org/article/how-to-get-started-tree-sitter
-
+;
+; But even if you do find a random treesitter parser, you need a Emacs Major Mode for it
+; see https://emacs.stackexchange.com/questions/78831/how-enable-tree-sitter-modes-ts-mode-that-arent-built-in
 
 (setq major-mode-remap-alist
  '(
