0dcdb6b12d5844d72686e732b32b4155440482fb
Ryan Wilcox <rwilcox@wilcoxd.com>
2024-02-01 21:48:02 -0500

    bring my sibling visit thing from OO -> here

--- functional_areas/markdown-mine.el
+++ functional_areas/markdown-mine.el
@@ -2,3 +2,17 @@
 
 (add-hook 'markdown-mode-hook 'imenu-add-menubar-index)
 (setq imenu-auto-rescan t)
+
+(defun rpw/presentation-next ()
+  "Let me quickly move from one focused section to another.
+
+  If I'm focused on a section of document, I may want to
+   easily move to a sibling of that document quickly.
+
+   Because sometimes I use outlines in place of slideshows"
+
+  (interactive)
+  (widen)
+  (markdown-outline-next-same-level)
+  (markdown-narrow-to-subtree)
+)
