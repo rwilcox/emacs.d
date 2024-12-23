538dc759517e70dbe974843e72160c0c6f09bcf0
Ryan Wilcox <rwilcox@wilcoxd.com>
2024-12-23 16:46:32 -0500

    refactor things so Emacs can boot again

--- init.el
+++ init.el
@@ -4,11 +4,13 @@
 		(concat user-emacs-directory
         (convert-standard-filename "mousemacs/")))
 
+
+
 (add-to-list 'load-path (expand-file-name "vendor" user-emacs-directory))
 (add-to-list 'load-path (expand-file-name "functional_areas" user-emacs-directory))
 (add-to-list 'load-path (expand-file-name "machine-configs" user-emacs-directory))
 
-(load (concat user-emacs-directory "boot-mine.el"))
+
 
 ;; load mousemacs
 (load (concat mousemacs-path "mousemacs-core.el"))
@@ -16,7 +18,11 @@
 (load (concat mousemacs-path "mousemacs-keybinding.el"))
 (load (concat mousemacs-path "mousemacs-etc.el"))
 
-(load (concat user-emacs-directory "packages-mine.el"))
 (load (concat user-emacs-directory "custom.el"))
+
+(load (concat user-emacs-directory "packages-mine.el"))
+(load (concat user-emacs-directory "boot-mine.el"))
+
+
 (load (concat user-emacs-directory "theme-mine.el"))
 (load (concat user-emacs-directory "boot-after-packages.el"))
