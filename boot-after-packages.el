836e6952f1d1399fafb03568be98375d5b8b7718
Ryan Wilcox <rwilcox@wilcoxd.com>
2026-01-06 15:38:45 -0500

    remove lsp references as I prefer eglot
    .... and I use my eglot-status function

--- boot-after-packages.el
+++ boot-after-packages.el
@@ -24,9 +24,6 @@
            "(%Z)"
             '(:eval (if (equal (car timeclock-last-event) "i") (format-time-string " IN @ %-I:%M %p"
 					                                           (nth 1 timeclock-last-event))))
-            '(:eval (if (bound-and-true-p lsp-mode)
-                           (propertize " | LSP: On" 'face '(:foreground "green"))
-                         (propertize " | LSP: Off" 'face '(:foreground "red"))))
 
 ))
 
@@ -36,7 +33,7 @@
 (global-set-key (kbd "C-x C-b") 'ibuffer)  ; UMMM YES No more buffer-mode
 
 
-
+(context-menu-mode)
 
 
 (provide 'boot-after-packages)
