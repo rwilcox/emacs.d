b740e0f124cfe808f3241677165b34959e9ae866
Ryan Wilcox <rwilcox@wilcoxd.com>
2026-02-05 19:45:40 -0500

    prettify org agenda

--- functional_areas/org-agendas-mine.el
+++ functional_areas/org-agendas-mine.el
@@ -40,3 +40,20 @@
             '(level 3))
       :sort nil
       :super-groups '((:auto-parent t)))))
+
+; I want org agenda to only show START times, not end times
+(add-hook 'org-agenda-finalize-hook
+          (lambda ()
+            (save-excursion
+              (goto-char (point-min))
+              (while (re-search-forward "\\([0-9]\\{2\\}:[0-9]\\{2\\}\\)-[0-9]\\{2\\}:[0-9]\\{2\\}" nil t)
+                (replace-match "\\1" nil nil)))))
+
+; and done todo items only show that they were completed, not when
+(add-hook 'org-agenda-finalize-hook
+          (lambda ()
+            (save-excursion
+              (goto-char (point-min))
+              ;; Remove time and "Closed:" label from closed items
+              (while (re-search-forward "^\\s-*[0-9]\\{2\\}:[0-9]\\{2\\}\\s-*┄+\\s-*Closed:\\s-+" nil t)
+                (replace-match "  ")))))
